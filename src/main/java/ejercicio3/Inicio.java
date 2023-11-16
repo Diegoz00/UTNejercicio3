bd67f740bf53d6e88da304350a04d9727df27c97
Diego Zalazar <diegozalazar00@gmail.com>
2023-11-16 10:26:40 -0300

    ejercicio3

--- src/main/java/ejercicio3/Inicio.java
+++ src/main/java/ejercicio3/Inicio.java
@@ -10,8 +10,9 @@ public class Inicio
     public static void main( String[] args ) {
 
         List<String> nombres = Arrays.asList("Java","JavaScript","Python","PHP","Nodejs");
+        int n = 3;
 
-        List<String> MayoresA = nombres.stream().filter(nombre -> nombre.length() > 5).collect(Collectors.toList());
+        List<String> MayoresA = nombres.stream().filter(nombre -> nombre.length() > n).collect(Collectors.toList());
 
         System.out.println(MayoresA);
 
